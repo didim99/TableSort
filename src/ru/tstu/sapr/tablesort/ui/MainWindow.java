d1e4efb2a549151d41e2a00c90290d073a15a7b1
IonShieldQuad <33602963+IonShieldQuad@users.noreply.github.com>
2018-10-05 23:39:40 +0300

    Added table display

--- src/ru/tstu/sapr/tablesort/ui/MainWindow.java
+++ src/ru/tstu/sapr/tablesort/ui/MainWindow.java
@@ -1,8 +1,11 @@
 package ru.tstu.sapr.tablesort.ui;
 
-import javax.swing.*;
-import java.util.ArrayList;
 import ru.tstu.sapr.tablesort.core.*;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+import java.util.ArrayList;
 
 public class MainWindow extends JFrame implements LogWriter {
   //Application level
@@ -16,6 +19,10 @@ public class MainWindow extends JFrame implements LogWriter {
   private JComboBox<String> selMethod;
   private JTable tArrayData;
   private JTable tInfo;
+  
+  private final int ARRAY_SIDE_LENGTH = 10;
+  private final int DATA_MIN = 0;
+  private final int DATA_MAX = 1000;
 
   public MainWindow(AppEventListener listener) {
     this.listener = listener;
@@ -42,6 +49,26 @@ public class MainWindow extends JFrame implements LogWriter {
       listener.onAppEvent(AppEvent.TEST_METHOD));
     btnTestAll.addActionListener(e ->
       listener.onAppEvent(AppEvent.TEST_ALL));
+  
+    //Sets model for array data table
+    tArrayData.setModel(new DefaultTableModel(ARRAY_SIDE_LENGTH, ARRAY_SIDE_LENGTH) {
+      @Override
+      public boolean isCellEditable(int row, int column) {
+        return false;
+      }
+    });
+    
+    //Sets renderer for array data table
+    TableCellRenderer r = new CustomTableCellRenderer(DATA_MIN, DATA_MAX);
+    tArrayData.setDefaultRenderer(Object.class, r);
+    
+    //Sets model for info table
+    tInfo.setModel(new DefaultTableModel(new String[]{"Sorting method", "Time"}, Model.SORT_METHODS_NAMES.length){
+      @Override
+      public boolean isCellEditable(int row, int column) {
+        return false;
+      }
+    });
   }
 
   public int getSortMethod() {
@@ -49,10 +76,19 @@ public class MainWindow extends JFrame implements LogWriter {
   }
 
   public void updateDataSet(int[] data) {
-
+    int cols = tArrayData.getModel().getColumnCount();
+    
+    for (int i = 0; i < data.length; ++i) {
+      int value = data[i];
+      tArrayData.getModel().setValueAt(value, i / cols, i % cols);
+    }
   }
 
   public void updateList(ArrayList<SortResult> results) {
-
+    for (int i = 0; i < results.size(); ++i) {
+      SortResult result = results.get(i);
+      tInfo.getModel().setValueAt(Model.SORT_METHODS_NAMES[result.getMethodIndex()], i, 0);
+      tInfo.getModel().setValueAt(result.getTime(), i, 1);
+    }
   }
 }
