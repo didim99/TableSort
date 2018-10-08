c17b69d42f06c44bdac9b181a6dfebabf41506a9
didim99 <didim@eclabs.ru>
2018-10-08 21:44:06 +0300

    + Implemented Bucket sort  + Added JAR manifest

--- src/ru/tstu/sapr/tablesort/core/sorter/BucketSorter.java
+++ src/ru/tstu/sapr/tablesort/core/sorter/BucketSorter.java
@@ -5,6 +5,23 @@ public class BucketSorter extends Sorter {
 
   @Override
   void sortInternal(int[] array) {
+    int maxValue = array[0];
 
+    for (int k = 1; k < array.length; k++)
+      if (array[k] > maxValue)
+        maxValue = array[k];
+
+    int i, j;
+
+    int[] bucket = new int[maxValue + 1];
+
+    for (i = 0; i < array.length; i++)
+      bucket[array[i]]++;
+
+    for (i = 0, j = 0; i < bucket.length; i++) {
+      for (; bucket[i] > 0; (bucket[i])--) {
+        array[j++] = i;
+      }
+    }
   }
 }
